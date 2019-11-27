48639ee7ac5824947400d8229c259fea6de5818d
fernandoors <fernando.ors@outlook.com>
2019-11-27 12:10:14 -0300

    Updates

---
+++ precache-manifest.980a080dc0f81b3c7b9a0c98bb2e4a92.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f70ac063878d7f90654179e90650ae",
+    "url": "/rsxp-front/index.html"
+  },
+  {
+    "revision": "796f5da3bc1e35839e9b",
+    "url": "/rsxp-front/static/js/2.03b54083.chunk.js"
+  },
+  {
+    "revision": "c08527b61775ff508a4f",
+    "url": "/rsxp-front/static/js/main.12f6eb90.chunk.js"
+  },
+  {
+    "revision": "13c135e7144f3a51fff9",
+    "url": "/rsxp-front/static/js/runtime-main.4c506f4e.js"
+  }
+]);
